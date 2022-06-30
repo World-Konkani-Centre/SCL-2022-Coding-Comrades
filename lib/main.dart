5007f3ff57e2576684d0429dd73e6533de721d6f
itsprathvi <prathviraj.is19@bmsce.ac.in>
2022-06-30 17:31:33 +0530

    Added UI for Authentication

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:authentication/login.dart';
+import 'package:authentication/signup.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'login',
+        routes: {
+          'login': (context) => const MyLogin(),
+          'register': (context) => const MyRegister(),
+        });
+  }
+}
