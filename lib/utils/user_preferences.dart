b43cfee1284281b3825513106adb6dc990c31ca9
SangeethaPrabhu2021 <1by20ai045@bmsit.ingit congig --gkobal user.name SangeethaPrabhu2021git config --global user.email 1by20ai045@bmsit.in>
2022-08-26 18:22:54 +0530

    ProfilePage_Code

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:user_profile_example/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+    name: 'Sarah Abs',
+    email: 'sarah.abs@gmail.com',
+    about:
+ 'Seeing for an entry-level position in a growing financial organization. Passionate about learning business concepts and possess the ability to handle work pressure and customer issues professionally. Have exceptional advisory skills to introduce new schemes and help customers to meet their financial goals.',
+    isDarkMode: true,
+  );
+}
